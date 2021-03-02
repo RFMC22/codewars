2c2f2e3194d6f2c13d733fe165441424fa562e51
Renee Magallanes <rene22797@gmail.com>
2021-03-02 10:52:42 -0500

    k8

---
+++ kyu8/solution7.js
@@ -0,0 +1,10 @@
+//Fake Binary
+function fakeBin(x){
+  return x.split('').map( int =>{
+    if(int < 5){
+      return '0';
+    }else{
+      return '1';
+    }
+  }).join('');
+}
---
+++ kyu8/solution8.js
@@ -0,0 +1,2 @@
+//Function 2 - squaring an argument
+let square = (num)=>{return Math.pow(num,2)};
