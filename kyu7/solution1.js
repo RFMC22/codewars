72e5a97dc539761d45421cdf74eb825c2fe6a14b
Renee Magallanes <rene22797@gmail.com>
2021-03-08 09:08:28 -0500

    kyu7

---
+++ kyu7/solution1.js
@@ -0,0 +1,10 @@
+//Beginner Series #3 Sum of Numbers
+function getSum(a,b){
+  if (a == b){
+    return a;
+  }else if (a > b){
+    return a + getSum(a-1,b)
+  } else {
+    return a + getSum(a+1,b)
+  }
+}
