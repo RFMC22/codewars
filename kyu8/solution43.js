702a0338d9faeb7716267bc111fda342e94c4921
Renee Magallanes <rene22797@gmail.com>
2021-04-06 19:14:18 -0500

    simple calculator

---
+++ kyu8/solution43.js
@@ -0,0 +1,12 @@
+//simple calculator
+function calculator(a,b,sign){
+  if(typeof a === 'number' && typeof b === 'number'){
+  switch(sign){
+      case '+': return a + b;
+      case '-': return a - b;
+      case '*': return a * b;
+      case '/': return a / b;
+      default: return "unknown value";
+    }
+  } return "unknown value";
+}
