85cb10ad5658b600a0d87dd0a41f7397c9dfdfec
Renee Magallanes <rene22797@gmail.com>
2021-04-11 12:04:14 -0500

    this is a problem

---
+++ kyu8/solution48.js
@@ -0,0 +1,8 @@
+// "this" is a problem
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  return {name: this.firstName + ' ' + this.lastName,
+         firstName: this.firstName,
+         lastName: this.lastName};
+}
