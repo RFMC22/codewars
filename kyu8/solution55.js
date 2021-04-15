268775cc8a7e0d0b638fa65fe7898b60c997a679
Renee Magallanes <rene22797@gmail.com>
2021-04-15 00:28:51 -0500

    Age Range Compatibility Equation

---
+++ kyu8/solution55.js
@@ -0,0 +1,12 @@
+//Age Range Compatibility Equation
+function datingRange(age){
+  if(age <= 14){
+    let min = Math.floor(age - 0.10 * age);
+    let max = Math.floor(age + 0.10 * age);
+    return `${min}-${max}`;
+  }else{
+    let min = Math.floor((age/2)+7);
+    let max = (age - 7) * 2;
+    return `${min}-${max}`;
+  }
+}
