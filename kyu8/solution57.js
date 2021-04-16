836e5815f15a804489a0c5afb705f56fc6a956fc
Renee Magallanes <rene22797@gmail.com>
2021-04-16 07:26:57 -0500

    Are You Playing Banjo?

---
+++ kyu8/solution57.js
@@ -0,0 +1,6 @@
+//Are You Playing Banjo?
+function areYouPlayingBanjo(name) {
+  return name.charAt(0) == 'R' || name.charAt(0) == 'r' 
+    ? `${name} plays banjo` 
+    : `${name} does not play banjo` ;
+}
