7b5bb28c3f851663c75eac206d546a0ea933a32f
Renee Magallanes <rene22797@gmail.com>
2021-04-17 12:22:34 -0500

    Basic subclasses - Adam and Eve

---
+++ kyu8/solution63.js
@@ -0,0 +1,18 @@
+//Basic subclasses - Adam and Eve
+class God{
+  /**
+   * @returns Human[]
+   */
+    static create(){
+      return [new Man(), new Woman()];
+    }
+  }
+  
+  class Human{ 
+  }
+  
+  class Man extends Human{
+  }
+  
+  class Woman extends Human{
+  }
