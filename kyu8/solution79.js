6e16be8d45bca5cf8c8ddd6bac97ce3edb3c41b6
Renee Magallanes <rene22797@gmail.com>
2021-04-22 08:45:32 -0500

    Broken Counter

---
+++ kyu8/solution79.js
@@ -0,0 +1,17 @@
+//Broken Counter
+class Counter{
+  constructor(){
+    this.value = 0;
+  }
+  increase() {
+  this.value++;
+};
+
+getValue() {
+  return this.value;
+};
+
+reset() {
+  this.value = 0;
+};
+}
