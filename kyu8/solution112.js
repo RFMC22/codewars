59048c5b86985bac8169c00c36ee7523c0a5c072
Renee Magallanes <rene22797@gmail.com>
2021-04-29 01:23:15 -0500

    Is this my tail?

---
+++ kyu8/solution112.js
@@ -0,0 +1,11 @@
+//Is this my tail?
+function correctTail(bod, tail){
+  let sub = bod.substr(bod.length-(tail.length));
+  
+  if (sub == tail){
+    return true
+  }
+  else {
+    return false
+  }   
+}
