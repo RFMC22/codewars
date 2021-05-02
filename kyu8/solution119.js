0941ad1abd6c5d8511ee7f7da50b2c614dd08d45
Renee Magallanes <rene22797@gmail.com>
2021-05-02 11:17:43 -0500

    I love you, a little , a lot, passionately ... not at all

---
+++ kyu8/solution119.js
@@ -0,0 +1,9 @@
+//I love you, a little , a lot, passionately ... not at all
+function howMuchILoveYou(nbPetals) {
+  let arr = ['I love you', 'a little', 'a lot', 'passionately', 'madly','not at all'];
+  for(let i = 0; i < nbPetals; i++)
+    {
+      arr.push(arr[i]);
+    }
+ return arr[nbPetals-1];
+}
