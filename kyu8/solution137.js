05e486aedb151239c2e99dfa4e7da027e36e10ec
Renee Magallanes <rene22797@gmail.com>
2021-05-11 14:34:44 -0500

    Find the position!

---
+++ kyu8/solution137.js
@@ -0,0 +1,5 @@
+//Find the position!
+function position(letter){
+  let arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  return `Position of alphabet: ${arr.indexOf(letter) + 1}`;
+}
