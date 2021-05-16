d7b17fb1f311527332aaf7b165d7d75fd1c9dab8
Renee Magallanes <rene22797@gmail.com>
2021-05-15 21:56:24 -0500

    Holiday VI - Shark Pontoon

---
+++ kyu8/solution145.js
@@ -0,0 +1,9 @@
+//Holiday VI - Shark Pontoon
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  if(dolphin)
+    {
+      sharkSpeed = sharkSpeed / 2;
+    }
+  
+  return  pontoonDistance / youSpeed < sharkDistance / sharkSpeed  ? 'Alive!' : 'Shark Bait!';
+}
