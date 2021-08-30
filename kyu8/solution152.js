45511916a3c120918629588fdf32325ac1ec05d8
Renee Magallanes <rene22797@gmail.com>
2021-08-30 08:06:38 -0500

    Find Multiples of a Number

---
+++ kyu8/solution152.js
@@ -0,0 +1,9 @@
+//Find Multiples of a Number
+function findMultiples(integer, limit){
+    let arr = [];
+    for(let i = integer; i<=limit; i+=integer){
+      a.push(i);
+    }
+    return arr
+  }
+  
