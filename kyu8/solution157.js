ee8e263259dee08473bca84fb4271201ff56a7b2
Renee Magallanes <rene22797@gmail.com>
2021-09-09 15:12:58 -0500

    Watermelon

---
+++ kyu8/solution157.js
@@ -0,0 +1,11 @@
+//Watermelon
+function divide(weight){
+    if(weight == 2){
+      return false;
+    }
+    else if(weight%2 == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
