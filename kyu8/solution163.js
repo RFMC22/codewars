e2ca48d0d01a83dae377a67348127452b4280873
Renee Magallanes <rmagallc@emeal.nttdata.com>
2022-06-16 20:32:04 -0500

    Training JS #3: Basic data types--String

---
+++ kyu8/solution163.js
@@ -0,0 +1,8 @@
+//Training JS #3: Basic data types--String
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n";
+let Dad = () => d1+a2+d2;
+let Bee = () => b1+e2+e2;
+let banana = () => b2+a2+n2+a2+n2+a2;
+let answer1 = () => "no" ;
+let answer2 = () => "no" ;
+let answer3 = () => "yes" ;
